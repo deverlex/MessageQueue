80b99dd2325826cd6dede9e3f7aa6c4ed59d40f7
Spider man <nguyendo94vn@gmail.com>
2018-08-19 13:57:18 +0700

    add model fanout and topic exchange

--- src/main/java/com/message/rabbitMQ/exchanges/FanoutExchange.java
+++ src/main/java/com/message/rabbitMQ/exchanges/FanoutExchange.java
@@ -1,4 +1,8 @@
 package com.message.rabbitMQ.exchanges;
 
 public class FanoutExchange extends Exchange {
+
+    public String getRoutingKey() {
+        return "";
+    }
 }
--- src/main/java/com/message/rabbitMQ/exchanges/TopicExchange.java
+++ src/main/java/com/message/rabbitMQ/exchanges/TopicExchange.java
@@ -2,4 +2,22 @@ package com.message.rabbitMQ.exchanges;
 
 public class TopicExchange extends Exchange {
 
+    private String routingKey;
+    private String routingPattern;
+
+    public String getRoutingKey() {
+        return routingKey;
+    }
+
+    public void setRoutingKey(String routingKey) {
+        this.routingKey = routingKey;
+    }
+
+    public String getRoutingPattern() {
+        return routingPattern;
+    }
+
+    public void setRoutingPattern(String routingPattern) {
+        this.routingPattern = routingPattern;
+    }
 }
