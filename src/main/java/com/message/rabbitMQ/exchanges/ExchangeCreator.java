b12ce27232b577290be4535eea93a17286e6d98c
Spider man <nguyendo94vn@gmail.com>
2018-08-19 11:51:55 +0700

    update exchange creator

--- src/main/java/com/message/rabbitMQ/exchanges/ExchangeCreator.java
+++ src/main/java/com/message/rabbitMQ/exchanges/ExchangeCreator.java
@@ -6,7 +6,6 @@ import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 
 import java.io.IOException;
-import java.util.Map;
 
 public class ExchangeCreator {
 
@@ -18,17 +17,9 @@ public class ExchangeCreator {
 
     public AMQP.Queue.BindOk createDirectExchange(DirectExchange directEx) throws IOException {
         Channel channel = rabbitMQClient.getChannel();
-        boolean exchangeExist = ExchangeUtils.isExchangeNameExist(channel, directEx.getExchangeName());
-        if (!exchangeExist) {
-            channel.exchangeDeclare(directEx.getExchangeName(), ExchangeType.DIRECT.type(), directEx.isDurable());
-        }
-
-        boolean queueExist = ExchangeUtils.isQueueExist(channel, directEx.getQueueName());
-        if (!queueExist) {
-            channel.queueDeclare(directEx.getQueueName(), directEx.isDurable(), directEx.isExclusive(),
+        channel.exchangeDeclare(directEx.getExchangeName(), ExchangeType.DIRECT.type(), directEx.isDurable());
+        channel.queueDeclare(directEx.getQueueName(), directEx.isDurable(), directEx.isExclusive(),
                     directEx.isAutoDelete(), directEx.getQueueArgs());
-        }
-
         return channel.queueBind(directEx.getQueueName(), directEx.getExchangeName(),
                 directEx.getRoutingKey(), directEx.getBindQueueArgs());
     }
